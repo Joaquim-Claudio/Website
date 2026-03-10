4adabd3607f3da5e5303da06a0685bd96d7abce1
Joaquim Cláudio <joaquim.claudio@izertis.com>
2026-03-10 14:25:49 +0000

    Project created

---
+++ src/components/Major.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Major() {
+    return (
+        <div>
+            <h1>Major Component</h1>
+            <p>Place major components like Navbar, Cards, Sections in this folder</p>
+        </div>
+    )
+}
+
+export default Major;
---
+++ src/components/ui/Minor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Minor() {
+    return (
+        <div>
+            <h1>Minor Component</h1>
+            <p>Place minor components like buttons, inputs, dropdowns in this folder</p>
+        </div>
+    )
+}
+
+export default Minor;
